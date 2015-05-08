153c4a4b93611cc515dc7656e9d445401199925c
R.J. Steinert <rj@rjsteinert.com>
2015-05-07 22:04:43 -0400

    Enable crossDomain for Backbone Models

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,20 +1,27 @@
 /*global App, $*/
 
-
 window.App = {
-    Models: {},
-    Collections: {},
-    Views: {},
-    Routers: {},
-    init: function () {
-        'use strict';
-        console.log('Hello from Backbone!');
-        var router = new App.Routers.Main()
-        Backbone.history.start()
-    }
-};
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  init: function () {
+    'use strict'
+    console.log('Hello from Backbone!')
+    var router = new App.Routers.Main()
+    Backbone.history.start()
+  }
+}
 
 $(document).ready(function () {
-    'use strict';
-    App.init();
-});
+  'use strict'
+  var proxiedSync = Backbone.sync
+  Backbone.sync = function(method, model, options) {
+    options || (options = {})
+    if (!options.crossDomain) {
+      options.crossDomain = true
+    }
+    return proxiedSync(method, model, options)
+  }
+  App.init();
+})
